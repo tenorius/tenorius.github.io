043cd95c2ba3ce3d05acf49fa7d4ecfe3b2aeab8
Rodrigo Oliveira Tenorio <rtenorio@uolinc.com>
2019-12-23 21:25:42 -0300

    first commit

---
+++ precache-manifest.0926e39f557290a693e345131f89d94e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca59bf59ad9e4c12ce255be1b08c3159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b264154332fee9e592e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ce672485a4ec66b0b9df",
+    "url": "/static/js/0.484c2f3a.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/static/js/0.484c2f3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "789c83fba1057b734bd2",
+    "url": "/static/js/3.54feaba7.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/3.54feaba7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e70e8699bdb0f8941cb6",
+    "url": "/static/js/4.08345af7.chunk.js"
+  },
+  {
+    "revision": "8da41a44ba96d0e03103",
+    "url": "/static/js/5.ddd9b80e.chunk.js"
+  },
+  {
+    "revision": "674b62f18fad90046796",
+    "url": "/static/js/6.1d8fdef8.chunk.js"
+  },
+  {
+    "revision": "6b264154332fee9e592e",
+    "url": "/static/js/main.a3361bf2.chunk.js"
+  },
+  {
+    "revision": "2091b2a14734149e2586",
+    "url": "/static/js/runtime-main.e1beb83f.js"
+  },
+  {
+    "revision": "edea891d05f695a8bb235fe0057223b7",
+    "url": "/static/media/chuck_norris_PNG1.edea891d.png"
+  }
+]);
